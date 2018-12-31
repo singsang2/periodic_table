45a8dc51ada8743483987efe5d2dc68a722d4def
Sung Bae <singsang2@Sungs-iMac.local>
2018-12-31 11:40:42 -0500

    initialize

---
+++ lib/periodic/table.rb
@@ -0,0 +1,8 @@
+require "periodic/table/version"
+
+module Periodic
+  module Table
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/periodic/table/version.rb
@@ -0,0 +1,5 @@
+module Periodic
+  module Table
+    VERSION = "0.1.0"
+  end
+end
